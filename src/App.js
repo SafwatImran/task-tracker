bfbd77e14ebbcb570ebb10ae7b177fd0f8fe0c21
Safwat Imran <safwatimran@gmail.com>
2021-03-01 16:38:01 +0600

    'usedLink'

--- src/App.js
+++ src/App.js
@@ -78,17 +78,17 @@ function App() {
       <Route path='/' exact render ={(props)=> 
         (
          <>
-            {showAddTask  && <AddTask onAdd={addTask}/>}
-            {tasks.length >0 ?
-            (<Tasks 
-              tasks={tasks} 
-              onDelete={deleteTask} 
-              onToggle={toggleReminder}
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ?
+              (<Tasks
+                tasks={tasks}
+                onDelete={deleteTask}
+                onToggle={toggleReminder}
               >
               </Tasks>)
-            : (
-              'No tasks to show'
-            )}  
+              : (
+                'No tasks to show'
+              )}  
          </> 
         )
     
