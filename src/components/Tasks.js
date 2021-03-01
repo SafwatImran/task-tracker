20bbd181a50485b10fe51fef79942e76f5be4404
Safwat Imran <safwatimran@gmail.com>
2021-03-01 16:04:36 +0600

    'mock_backed'

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,8 @@ import Task from './Task'
 const Tasks = ({tasks, onDelete, onToggle})=>{
     return ( 
         <>
-        {tasks.map((task)=> (<Task onToggle={onToggle} key = {task.id} task={task} onDelete={onDelete}/>))}
+        {tasks.map((task, index)=> 
+        (<Task onToggle={onToggle} key = {index} task={task} onDelete={onDelete}/>))}
         
         </>
     )
